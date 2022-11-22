fe0364fe2ff586653335a1d71d580ba2c2d00e4f
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2022-11-22 15:22:22 -0500

    pushing up populate, continue to think about

--- memory_puzzle/board.rb
+++ memory_puzzle/board.rb
@@ -1,10 +1,11 @@
 require_relative 'card.rb'
+require 'byebug'
 class Board
     attr_reader :size
     def initialize(n = 4)
         raise "board must be even >:(!!!" if n.odd?
         raise "board must have less than 52 spaces!!" if n > 6 
-        @grid = Array.new(n) { Array.new(n) }
+        @grid = Array.new(n) { Array.new(n, Card.new(nil)) }
         @size = n * n
     end
 
@@ -13,17 +14,24 @@ class Board
         remaining_pairs = @size / 2
         max = @grid.length
         card_placed = 0
+
         while remaining_pairs > 0
-            current_card = alphabet_whatever[rand(0 ... alphabet_whatever.length)]
+            current_card = alphabet_whatever[rand(alphabet_whatever.length)]
             alphabet_whatever[alphabet_whatever.index(current_card)] = ""
+            puts "current card" + current_card.to_s
             while card_placed < 2
-                samp_row, samp_col = (0...max).rand, (0...max).rand
-                if @grid[samp_row][samp_col] == nil
+                samp_row, samp_col = rand(max), rand(max)
+            
+                if @grid[samp_row][samp_col] == Card.new(nil)
+                    puts "row, col" + samp_row.to_s + samp_col.to_s
                     @grid[samp_row][samp_col] = Card.new(current_card)
                     card_placed += 1
+                    puts "card placed" + card_placed.to_s
                 end
-                remaining_pairs -= 1
             end
+            remaining_pairs -= 1
+            puts "remaining pairs" + remaining_pairs.to_s
+            card_placed = 0
         end
     end
 
